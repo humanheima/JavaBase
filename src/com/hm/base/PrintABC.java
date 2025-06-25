e4e030fd5dd8d5fc02bed9ece93c7cc0ba6af670
dumingwei <dumingwei@zhumengdao.com>
2025-06-25 16:28:14 +0800

    使用信号量，Semaphore 交替打印 ABC 。

---
+++ src/com/hm/base/PrintABC.java
@@ -0,0 +1,90 @@
+package com.hm.base;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by p_dmweidu on 2025/6/25
+ * Desc:
+ */
+public class PrintABC {
+
+    private int n; // 循环次数
+    private Semaphore semA = new Semaphore(1); // 控制打印 A
+    private Semaphore semB = new Semaphore(0); // 控制打印 B
+    private Semaphore semC = new Semaphore(0); // 控制打印 C
+
+    public PrintABC(int n) {
+        this.n = n;
+    }
+
+    // 打印 A 的线程
+    public void printA() throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            semA.acquire(); // 获取 A 的信号量
+            System.out.print("A");
+            semB.release(); // 释放 B 的信号量
+        }
+    }
+
+    // 打印 B 的线程
+    public void printB() throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            semB.acquire(); // 获取 B 的信号量
+            System.out.print("B");
+            semC.release(); // 释放 C 的信号量
+        }
+    }
+
+    // 打印 C 的线程
+    public void printC() throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            semC.acquire(); // 获取 C 的信号量
+            System.out.print("C");
+            semA.release(); // 释放 A 的信号量
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5; // 打印 3 次 ABC
+        PrintABC printer = new PrintABC(n);
+
+        // 创建三个线程
+        Thread tA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    printer.printA();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread tB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    printer.printB();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread tC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    printer.printC();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // 启动线程
+        tA.start();
+        tB.start();
+        tC.start();
+    }
+}
---
+++ src/com/hm/base/PrintZeroEvenOdd.java
@@ -0,0 +1,105 @@
+package com.hm.base;
+
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+/**
+ * Created by p_dmweidu on 2025/6/25
+ * Desc:三个线程分别打印 0、奇数和偶数，输出格式为 01020304...。
+ */
+public class PrintZeroEvenOdd {
+    private int n;
+
+
+    private Semaphore zeroSem = new Semaphore(1);  // 控制打印0
+    private Semaphore evenSem = new Semaphore(0);  // 控制打印偶数
+    private Semaphore oddSem = new Semaphore(0);   // 控制打印奇数
+
+    public PrintZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // 打印0的线程
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            //获取一个许可，减少计数器。如果计数 < 0，线程阻塞。
+            zeroSem.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 1) {
+                //释放一个许可，增加计数器，并唤醒一个等待线程（如果有）。
+                oddSem.release();  // 奇数轮次释放奇数信号量
+            } else {
+                //释放一个许可，增加计数器，并唤醒一个等待线程（如果有）。
+                evenSem.release(); // 偶数轮次释放偶数信号量
+            }
+        }
+    }
+
+    // 打印偶数的线程
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            evenSem.acquire();
+            printNumber.accept(i);
+            zeroSem.release(); // 释放给打印0的线程
+        }
+    }
+
+    // 打印奇数的线程
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            oddSem.acquire();
+            printNumber.accept(i);
+            zeroSem.release(); // 释放给打印0的线程
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5; // 示例输入
+        PrintZeroEvenOdd printer = new PrintZeroEvenOdd(n);
+        IntConsumer printNumber = new IntConsumer() {
+            @Override
+            public void accept(int value) {
+                System.out.print(value);
+            }
+        };
+
+        // 创建三个线程
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    printer.zero(printNumber);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    printer.odd(printNumber);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    printer.even(printNumber);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // 启动线程
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
